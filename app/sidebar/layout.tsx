47c8d289a67519f93236704c672029c366c8c222
Sirjoshodevelops <info@sitely.agency>
2025-07-21 11:40:08 +0200

    Remove Navigation from Sidebar Layout

--- app/sidebar/layout.tsx
+++ app/sidebar/layout.tsx
@@ -17,8 +17,3 @@ export default function SidebarLayout({children}: Readonly<{ children: React.Rea
             disableTransitionOnChange
         >
             {children}
-        </ThemeProvider>
-        </body>
-        </html>
-    );
-}
